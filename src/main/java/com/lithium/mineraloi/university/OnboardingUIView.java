9b66705a43ec1e03bd526d21eb62c142621ee4af
Josephine Cortez-Ayala <josephine.cortez-ayala@khoros.com>
2019-07-01 13:15:45 -0500

    adding files back

---
+++ src/main/java/com/lithium/mineraloi/university/OnboardingUIView.java
@@ -0,0 +1,29 @@
+package com.lithium.mineraloi.university;
+
+import com.lithium.mineraloi.university.browser.DriverManager;
+import com.lithium.mineraloil.selenium.elements.BaseElement;
+import com.lithium.mineraloil.selenium.elements.Driver;
+import com.lithium.mineraloil.selenium.elements.TextElement;
+import org.openqa.selenium.By;
+
+import java.util.List;
+
+public class OnboardingUIView {
+    private Driver driver;
+
+    public OnboardingUIView() {
+        driver = DriverManager.INSTANCE.getDriver();
+    }
+
+    public TextElement getFilterInputElement() {
+        return driver.createTextElement(By.xpath("//input[@id='filterInput']"));
+    }
+
+    public BaseElement getButtonElement() {
+        return driver.createBaseElement(By.xpath("//button[@id='filterButton']"));
+    }
+
+    public List<TextElement> getStatusElement() {
+        return driver.createTextElement(By.xpath("//p[@class='tweetText']")).toList();
+    }
+}
